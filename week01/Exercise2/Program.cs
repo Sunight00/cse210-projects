1a2273f7bd4a698ca8d7fba804bb6af585d71f79
Sunight00 <ephraim.ewane@gmail.com>
2025-01-09 12:06:57 +0100

    changes

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Your Grade:");
+        string g = Console.ReadLine();
+        // converting g (grade) to integer
+        int grade = int.Parse(g);
+
+        if (grade>=90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (grade>=80)
+        {
+            Console.WriteLine("B");
+        }
     }
 }
