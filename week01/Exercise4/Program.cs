fd585498916c1fa8f60aa223b9d78d2c984f09b8
Sunight00 <ephraim.ewane@gmail.com>
2025-01-09 19:48:05 +0100

    changes

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,14 +1,36 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
         List<int> numbers = new List<int>();
-        //created a list for user input
+        //Above created a list for user input
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         Console.Write("Enter a number: ");
         string value = Console.ReadLine();
         int number = int.Parse(value);
-        numbers.Add(number);
+        // Above coverted the input to an int
+
+        while (number !=0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter a number: ");
+            value = Console.ReadLine();
+            number = int.Parse(value);
+        }
+        //Above is a loop that end when 0 is inputed and it appends other figures to list
+        int sum=0;
+        int average=0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+            average = sum / numbers.Count;
+        }
+        Console.WriteLine($"Sum is {sum}");
+        Console.WriteLine($"Sum is {average}");
+        Console.WriteLine($"Average {numbers.max()}")
+        
     }
 }
