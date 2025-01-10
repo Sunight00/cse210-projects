1725621a37d240969ff805a7009752a5e2412a65
Sunight00 <ephraim.ewane@gmail.com>
2025-01-10 22:50:49 +0100

    done

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void welcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string name()
+        {
+            Console.Write("Please enter your name: ");
+            string user = Console.ReadLine();
+            return user;
+        }
+        static int number()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int favnum = int.Parse(Console.ReadLine()); // played around with this line to get it to work
+            return favnum;
+        }
+        static void result(string user, int favnum)
+        {
+            int square = favnum*favnum;
+            Console.WriteLine($"{user}, the square of your number is {square} .");
+        }
+        welcome();
+        string person =name();
+        int num = number();
+        result(person, num);
     }
 }
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,30 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        static void welcome()
-        {
-            Console.WriteLine("Welcome to the Program!");
-        }
-        static string name()
-        {
-            Console.Write("Please enter your name: ");
-            string user = Console.ReadLine();
-            return user;
-        }
-        static int number()
-        {
-            Console.Write("Please enter your favorite number: ");
-            int favnum = int.Parse(Console.ReadLine()); // played around with this line to get it to work
-            return favnum;
-        }
-        static void result(string user, int favnum)
-        {
-            int square = favnum*favnum;
-            Console.WriteLine($"{user}, the square of your number is {square} .");
-        }
-        welcome();
-        string person =name();
-        int num = number();
-        result(person, num);
+        Console.WriteLine("Hello World! This is the week 5 exercise.");
     }
 }
