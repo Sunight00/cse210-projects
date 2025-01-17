df273df20fcf892e98b7c55cf4a514081f5e5916
Sunight00 <ephraim.ewane@gmail.com>
2025-01-17 23:18:41 +0100

    sighs

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -4,7 +4,7 @@ public class PromptGenerator
     
 
     public string GetRandomPrompt()
-    {
+    {   /*appends the strings to the list _prompt*/
         _prompt.Add("What was the best part of your day?");
         _prompt.Add("What was the strongest emotion you felt today?");
         _prompt.Add("Made anyone happy today?");
@@ -12,6 +12,8 @@ public class PromptGenerator
         _prompt.Add("Who was the most interesting person you interacted with today?");
         _prompt.Add("What do you think you can do better from today?");
 
+        //this counts the number of strings in the list, after getting a number it selects a random 
+        //number [index] to pick the string for display.
         Random  rmd = new Random();
         int index = rmd.Next(_prompt.Count);
         string randomPrompt=_prompt[index];
