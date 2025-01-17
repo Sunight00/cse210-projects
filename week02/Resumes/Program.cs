c74f38bbd3c090af92ec28726a1a83fead621dd6
Sunight00 <ephraim.ewane@gmail.com>
2025-01-17 14:55:21 +0100

    changes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,21 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        public class Job
-    {
-            public string _company ="";
-            public string _jobTile ="";
-            public int _startYear =0;
-             public int _endYear =0;
-
-            Job job1 = new Job;
-            job1._jobTitle = "Software Engineer";
-
-            public void display()
-            {
-                Console.WriteLine($"{_jobTile} ({_company}) {_startYear}-{_endYear}");
-                
-            }
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
     }
-        
-    }
+}
