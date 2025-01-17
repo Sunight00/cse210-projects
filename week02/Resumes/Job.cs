7b1f1a50e9c3bf089e029b32413a923395b42434
Sunight00 <ephraim.ewane@gmail.com>
2025-01-17 15:27:52 +0100

    c

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,13 +1,14 @@
 using System;
+
 public class Job
 {
+    public string _jobTitle;
     public string _company;
-    public string _jobTile;
     public int _startYear;
     public int _endYear;
 
     public void Display()
     {
-        Console.WriteLine($"{_jobTile} ({_company}) {_startYear}-{_endYear}");
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
