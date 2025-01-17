e821cd301fc6367b828935d08b78b59cc26cf9b9
Sunight00 <ephraim.ewane@gmail.com>
2025-01-17 21:45:26 +0100

    sighs

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,12 +1,11 @@
 public class PromptGenerator
 {
-    public List<Entry> _prompt;
     public List<string> _prom;
 
     public string GetRandomPrompt()
     {
         Random  rmd = new Random();
-        int
+        int index 
         
         return "";
 
