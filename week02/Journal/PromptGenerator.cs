202f7b3608588ad2403dffd8c417159cc242a94f
Sunight00 <ephraim.ewane@gmail.com>
2025-01-17 21:58:43 +0100

    okay

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,21 +1,19 @@
 public class PromptGenerator
 {
     public List<string> _prompt = new List<string>();
-    _prompt.Add("What was the best part of my day?")
+    
 
    
 
     public string GetRandomPrompt()
     {
+        _prompt.Add("What was the best part of my day?");
+        _prompt.Add("What was the best part of my day?");
+
         Random  rmd = new Random();
         int index = rmd.Next(_prompt.Count);
-        
-        
-        
-        return "";
-
-
-
-        
+        string ran=_prompt[index];
+        return ran;
+  
     }
 }
