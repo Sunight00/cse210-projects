6af56d3230972cd0c3e96da7b64707ab87e3c838
Sunight00 <ephraim.ewane@gmail.com>
2025-01-17 21:05:39 +0100

    a

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -6,10 +6,8 @@ class Program
     {
         Entry newEntry = new Entry();
         newEntry._date="1,11";
-        newEntry._promptText="sjsjjs";
+        newEntry._promptText=
         newEntry._entryText="sjsjsjsjsjsjs";
-
-
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        newEntry.Display();
     }
 }
