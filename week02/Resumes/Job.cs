90b82081a9fc6f761bd42616d5c3910f2d2d9d7e
Sunight00 <ephraim.ewane@gmail.com>
2025-01-17 14:50:11 +0100

    changes

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,13 @@
+public class Job
+{
+    public string _company ="";
+    public string _jobTile ="";
+    public int _startYear =0;
+    public int _endYear =0;
+
+
+    public void display()
+    {
+        Console.WriteLine($"{_jobTile} ({_company}) {_startYear}-{_endYear}");
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
+        public class Job
+    {
+            public string _company ="";
+            public string _jobTile ="";
+            public int _startYear =0;
+             public int _endYear =0;
+
+            Job job1 = new Job;
+            job1._jobTitle = "Software Engineer";
+
+            public void display()
+            {
+                Console.WriteLine($"{_jobTile} ({_company}) {_startYear}-{_endYear}");
+                
+            }
+    }
         
     }
-}
