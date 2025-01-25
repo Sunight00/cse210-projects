a586c97c1fca4c14615078b5c0b1877e4af962ed
Sunight00 <ephraim.ewane@gmail.com>
2025-01-25 23:16:02 +0100

    r

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        string v = "ffff";
+        string abc = new string('_', v.Length);
+        
+        Console.WriteLine(abc);
     }
 }
