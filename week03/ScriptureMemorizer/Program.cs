9eec32609feac948dedb32d31e79bc5d026f9317
Sunight00 <ephraim.ewane@gmail.com>
2025-01-26 01:12:34 +0100

    c

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,15 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        string reespone ="";
-        while ( string response=="quit")
+        
+        while (true)
         {
             Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
             Scripture n = new Scripture();
             n.HideWords();
             Console.WriteLine(n.GetDisplayText());
         }
-        string response = Console.Readline();
+        //string response = Console.Readline();
          
         
 
