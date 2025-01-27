c7c290bb3fbe7cc6a30790d763e2a1f8fb850078
Sunight00 <ephraim.ewane@gmail.com>
2025-01-27 23:14:13 +0100

    x

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        int vid = 0;
+        List <string> vidTitle = ["Dance","Home school"];
+        List<string> vidAuthor = ["James","Holland"];
+        List<double> vidLength = [10,29];
+
+        List<string> commentorName = ["henry","Effy"];
+        
+
+        while ( vid !=4)
+        {
+
+        }
     }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -3,7 +3,14 @@ public class Video
     public string _title;
     public string _author;
     public double _length;
-    public List<string> _comments;
+    public List<string> _comments = new List<string>();
+
+    public Video()
+    {
+        Comment n = new Comment();
+        
+        _comments.Add(n._text);
+    }
 
     public int TotalComments()
     {
