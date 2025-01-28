e0c24cab4b6210343269fe81d051eb1872f73ce2
Sunight00 <ephraim.ewane@gmail.com>
2025-01-28 13:08:05 +0100

    n

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,23 @@
+public class Address
+{
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
+
+    public bool IsUsa()
+    {
+        if (_country == "USA")
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public string CustomerAddress()
+    {
+        return $"{_street},{_city},{_state},{_country}";
+    }
+}
