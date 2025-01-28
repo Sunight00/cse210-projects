88cdd9895a09f14586057457a23ce7958787f2b5
Sunight00 <ephraim.ewane@gmail.com>
2025-01-28 01:05:48 +0100

    j

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -6,11 +6,21 @@ class Program
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
         
-  
-        int vid = 0;
-        while ( vid !=4)
-        {
+        Video vid1 = new Video("Horror","Jake Cage",12);
+        Video vid2 = new Video("Music fest","Luck Matt",3);
+        Video vid3 = new Video("Dance again","Collins Fred",4);
+        Video vid4 = new Video("Move Up","Smith Williams",5);
+        
+        List<Video> youtube = new List<Video>();
+        youtube.Add(vid1);
+        youtube.Add(vid2);
+        youtube.Add(vid3);
+        youtube.Add(vid4);
 
+
+        foreach(var vid in youtube)
+        {
+            
         }
     }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -5,9 +5,11 @@ public class Video
     public double _length;
     public List<string> _comments = new List<string>();
 
-    public Video()
+    public Video(string setTitle, string setAuthor, double setLength)
     {
-        
+        _title=setTitle;
+        _author=setAuthor;
+        _length=setLength;
     }
 
     public int TotalComments()
