884f68d02c14c59a621bccde509aa6041b2ff566
Sunight00 <ephraim.ewane@gmail.com>
2025-02-07 22:32:44 +0100

    ss

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,22 @@
+namespace sport
+{
+    public class Activity
+    {
+        public void DisplayStartingMessage()
+        {
+
+        }
+        public void DisplayEndingMessage()
+        {
+
+        }
+        public void ShowSpinner(int seconds)
+        {
+
+        }
+        public void ShowCountDown(int second)
+        {
+            
+        }
+    }
+}
