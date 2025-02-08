a7912e5b075168e34c5b134da675832b845cc739
Sunight00 <ephraim.ewane@gmail.com>
2025-02-09 00:01:11 +0100

    x

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -32,7 +32,7 @@ class Program
 
                 Console.Clear();
                 Console.WriteLine("Get ready....");
-                activity.ShowSpinner(5);
+                activity.ShowSpinner(1);
 
                 activity.ShowCountDown(duration);
           
