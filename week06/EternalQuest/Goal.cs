0c337213322e9c694c4f21497999ca709bdee2d7
Sunight00 <ephraim.ewane@gmail.com>
2025-02-16 20:27:04 +0100

    ff

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,36 @@
+using System.Runtime.CompilerServices;
+
+namespace Goal{
+    public abstract class Goal
+    {
+        protected string _shortname;
+        protected string _description;
+        protected int _points;
+
+        public Goal(string name, string description, int points)
+        {
+            _shortname = name;
+            _description =description;
+            _points = points;
+        }
+
+
+
+
+        public abstract void RecordEvent();
+
+        public abstract bool IsComplete();
+
+
+
+        public string GetDetails()
+        {
+            return "";
+        }
+
+
+        public abstract string GetStringRepresentation();
+
+
+    }
+}
---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,30 @@
+namespace Goal
+{
+    public class SimpleGoal:Goal
+    {
+        private bool _IsComplete;
+
+        public SimpleGoal(string name, string description, int points):base(name,description,points)
+        {
+             _shortname = name;
+            _description =description;
+            _points = points;
+        }
+
+        public override void RecordEvent()
+        {
+
+        }
+
+        public override string GetStringRepresentation()
+        {
+            return $"{_shortname}: {_description} - {_points} points";
+        }
+
+        public override bool IsComplete()
+        {
+            return _IsComplete;
+        }
+    }
+
+}   
