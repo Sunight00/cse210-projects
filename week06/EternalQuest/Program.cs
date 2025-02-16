65b3f4a6720d4203f23f1b8e83cfd0c237fb9451
Sunight00 <ephraim.ewane@gmail.com>
2025-02-16 22:40:39 +0100

    d

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -7,5 +7,18 @@ class Program
     {
         GoalManager goalManager = new GoalManager();
         goalManager.DisplayPlayerInfo();
+
+        while (true)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Create New Goals");
+            Console.WriteLine("2. List Goal");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Quit");
+            Console.Write("Select a choice from the options");
+            string option = Console.ReadLine();
+        }
     }
 }
