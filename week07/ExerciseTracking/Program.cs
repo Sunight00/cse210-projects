1283186d64ffa5f4e81b63934640c3f3ae916494
Sunight00 <ephraim.ewane@gmail.com>
2025-02-18 13:44:59 +0100

    ss

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -8,9 +8,13 @@ namespace Sport
     {
         Running run = new Running("4th Nov 2033",30,3);
         Console.WriteLine(run.GetSummary());
-
+    Console.WriteLine();
         Cycling cycling = new Cycling("4th Nov 2033",30,12);
         Console.WriteLine(cycling.GetSummary());
+    Console.WriteLine();
+        Swimming swimming = new Swimming("4th Nov 2033",30,20);
+        Console.WriteLine(swimming.GetSummary());
+    
     }
 }
 }
